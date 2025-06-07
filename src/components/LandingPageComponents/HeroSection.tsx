0494a7480d3462b505565b1023370a71968cda3e
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-07 14:23:01 +0200

    Add Custom container

--- src/components/LandingPageComponents/HeroSection.tsx
+++ src/components/LandingPageComponents/HeroSection.tsx
@@ -4,43 +4,45 @@ export function HeroSection() {
   return (
     <>
       <section className="bg-white p-15">
-        {/* Descriptions */}
-        <div className="text-center">
-          <h1 className="text-6xl font-bold">Find Your Dream Job</h1>
-          <span className="text-6xl font-bold text-sky-500">Today</span>
-          <p className="text-2xl mt-5 text-gray-600">
-            Connect with thousands of companies hiring now. Your next career
-            opportunity is just a search away.
-          </p>
-        </div>
-
-        {/* Find Job */}
-        <div className="flex bg-white mt-12 gap-3 rounded-full border border-gray-100 items-center pr-2">
-          {/* Job Title Input */}
-          <div className="relative w-1/2">
-            <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-6 h-6 pointer-events-none" />
-            <input
-              type="text"
-              placeholder="Job Title"
-              className="w-full pl-12 p-5 outline-0 rounded-lg border-gray-200 text-lg"
-            />
+        <div className="custum-container">
+          {/* Descriptions */}
+          <div className="text-center">
+            <h1 className="text-6xl font-bold">Find Your Dream Job</h1>
+            <span className="text-6xl font-bold text-sky-500">Today</span>
+            <p className="text-2xl mt-5 text-gray-600">
+              Connect with thousands of companies hiring now. Your next career
+              opportunity is just a search away.
+            </p>
           </div>
 
-          {/* Separator */}
-          <div className="h-10 w-0.5 bg-gray-400"></div>
+          {/* Find Job */}
+          <div className="flex bg-white mt-12 gap-3 rounded-full border border-gray-100 items-center pr-2">
+            {/* Job Title Input */}
+            <div className="relative w-1/2">
+              <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-6 h-6 pointer-events-none" />
+              <input
+                type="text"
+                placeholder="Job Title"
+                className="w-full pl-12 p-5 outline-0 rounded-lg border-gray-200 text-lg"
+              />
+            </div>
 
-          {/* Location Input */}
-          <div className="relative w-1/2">
-            <MapPin className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-6 h-6 pointer-events-none" />
-            <input
-              type="text"
-              placeholder="Location"
-              className="w-full pl-12 p-5 outline-0 rounded-lg border-gray-200 text-lg"
-            />
+            {/* Separator */}
+            <div className="h-10 w-0.5 bg-gray-400"></div>
+
+            {/* Location Input */}
+            <div className="relative w-1/2">
+              <MapPin className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-6 h-6 pointer-events-none" />
+              <input
+                type="text"
+                placeholder="Location"
+                className="w-full pl-12 p-5 outline-0 rounded-lg border-gray-200 text-lg"
+              />
+            </div>
+            <button className="cursor-pointer w-35 p-3 rounded-3xl bg-sky-500 text-white text-lg hover:bg-sky-500/90">
+              Search
+            </button>
           </div>
-          <button className="cursor-pointer w-35 p-3 rounded-3xl bg-sky-500 text-white text-lg hover:bg-sky-500/90">
-            Search
-          </button>
         </div>
       </section>
     </>
--- src/components/LandingPageComponents/SolutionsSection.tsx
+++ src/components/LandingPageComponents/SolutionsSection.tsx
@@ -13,64 +13,68 @@ import { Solution } from "../Solution";
 export function Solutions() {
   return (
     <section className="p-20 bg-slate-100">
-      <div>
-        {" "}
-        {/* Title */}
-        <div className="text-center font-bold text-4xl tracking-wide">
-          <h2>One Platform</h2>
-          <h2>
-            Many <span className="text-sky-500">Solutions</span>
-          </h2>
-        </div>
-        {/* Solutions */}
-        <div className="grid grid-cols-4 gap-5 mt-10">
-          <Solution
-            solutionName="Marketing & Communication"
-            jobsCount={12}
-            icon={<MicVocal color="#e53935" width="25px" height="25px" />}
-          />
+      <div className="custom-container
+      
+      ">
+        <div>
+          {" "}
+          {/* Title */}
+          <div className="text-center font-bold text-4xl tracking-wide">
+            <h2>One Platform</h2>
+            <h2>
+              Many <span className="text-sky-500">Solutions</span>
+            </h2>
+          </div>
+          {/* Solutions */}
+          <div className="grid grid-cols-4 gap-5 mt-10">
+            <Solution
+              solutionName="Marketing & Communication"
+              jobsCount={12}
+              icon={<MicVocal color="#e53935" width="25px" height="25px" />}
+            />
 
-          <Solution
-            solutionName="Design & Development"
-            jobsCount={12}
-            icon={<PaintBucket color="#7b1fa2" width="25px" height="25px" />}
-          />
+            <Solution
+              solutionName="Design & Development"
+              jobsCount={12}
+              icon={<PaintBucket color="#7b1fa2" width="25px" height="25px" />}
+            />
 
-          <Solution
-            solutionName="Human Research & Development"
-            jobsCount={12}
-            icon={<EarthLock color="#2979ff" width="25px" height="25px" />}
-          />
+            <Solution
+              solutionName="Human Research & Development"
+              jobsCount={12}
+              icon={<EarthLock color="#2979ff" width="25px" height="25px" />}
+            />
 
-          <Solution
-            solutionName="Finance & Management"
-            jobsCount={12}
-            icon={<HandCoins color="#ffab40" width="25px" height="25px" />}
-          />
+            <Solution
+              solutionName="Finance & Management"
+              jobsCount={12}
+              icon={<HandCoins color="#ffab40" width="25px" height="25px" />}
+            />
 
-          <Solution
-            solutionName="Armforce Guide & Security"
-            jobsCount={12}
-            icon={<ShieldCheck color="#6200ea" width="25px" height="25px" />}
-          />
+            <Solution
+              solutionName="Armforce Guide & Security"
+              jobsCount={12}
+              icon={<ShieldCheck color="#6200ea" width="25px" height="25px" />}
+            />
 
-          <Solution
-            solutionName="Business & Consulting"
-            jobsCount={12}
-            icon={<Handshake color="#29b6f6" width="25px" height="25px" />}
-          />
+            <Solution
+              solutionName="Business & Consulting"
+              jobsCount={12}
+              icon={<Handshake color="#29b6f6" width="25px" height="25px" />}
+            />
 
-          <Solution
-            solutionName="Customer Support Care"
-            jobsCount={12}
-            icon={<MicVocal color="#009688" width="25px" height="25px" />}
-          />
+            <Solution
+              solutionName="Customer Support Care"
+              jobsCount={12}
+              icon={<MicVocal color="#009688" width="25px" height="25px" />}
+            />
 
-          <Solution
-            solutionName="Project Management"
-            jobsCount={12}
-            icon={<FolderKanban color="#e040fb" width="25px" height="25px" />}
-          />
+            <Solution
+              solutionName="Project Management"
+              jobsCount={12}
+              icon={<FolderKanban color="#e040fb" width="25px" height="25px" />}
+            />
+          </div>
         </div>
       </div>
     </section>
