358c1d68a9e02522e229be4100af2ba07295cca7
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-07 17:13:44 +0200

    Global Update

--- src/components/LandingPageComponents/Header.tsx
+++ src/components/LandingPageComponents/Header.tsx
@@ -1,46 +1,48 @@
 export function Header() {
   return (
-    <div className="custum-container">
-      <header className="flex justify-between items-center p-2.5 shadow-lg bg-white sticky top-0 z-1000">
-        {/* Logo */}
-        <div className="font-bold text-2xl text-sky-500">UpLink</div>
+    <header className="p-2.5 shadow-lg bg-white sticky top-0 z-1000">
+      <div className="custum-container">
+        <div className="flex justify-between items-center">
+          {/* Logo */}
+          <div className="font-bold text-2xl text-sky-500">UpLink</div>
 
-        {/* Navigation */}
-        <nav>
-          <ul className="flex text-lg">
-            <li>
-              <a className="nav-ele" href="">
-                Find Jobs
-              </a>
-            </li>
-            <li>
-              <a className="nav-ele" href="">
-                Companies
-              </a>
-            </li>
-            <li>
-              <a className="nav-ele" href="">
-                Contact
-              </a>
-            </li>
-            <li>
-              <a className="nav-ele" href="">
-                About
-              </a>
-            </li>
-          </ul>
-        </nav>
+          {/* Navigation */}
+          <nav>
+            <ul className="flex text-lg">
+              <li>
+                <a className="nav-ele" href="">
+                  Find Jobs
+                </a>
+              </li>
+              <li>
+                <a className="nav-ele" href="">
+                  Companies
+                </a>
+              </li>
+              <li>
+                <a className="nav-ele" href="">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a className="nav-ele" href="">
+                  About
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-        {/* Actions */}
-        <div>
-          <button className="mr-4 pl-5 pr-5 pt-3 pb-3 rounded-md hover:bg-blue-50 hover:text-sky-600 font-medium">
-            Log in
-          </button>
-          <button className="pl-5 pr-5 pt-3 pb-3 rounded-md bg-sky-600 text-white font-medium">
-            Register
-          </button>
+          {/* Actions */}
+          <div>
+            <button className="mr-4 pl-5 pr-5 pt-3 pb-3 rounded-md hover:bg-blue-50 hover:text-sky-600 font-medium">
+              Log in
+            </button>
+            <button className="pl-5 pr-5 pt-3 pb-3 rounded-md bg-sky-600 text-white font-medium">
+              Register
+            </button>
+          </div>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 }
