15f1c47f5dc8e0c7905bc7cf512709182c0705a1
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-08 14:09:07 +0200

    Add Job Props Typ

---
+++ src/types/ISkill.tsx
@@ -0,0 +1,4 @@
+export interface ISkill {
+  SkillId: number;
+  Skill: string;
+}
---
+++ src/types/JobProps.tsx
@@ -0,0 +1,10 @@
+import type { ISkill } from "./ISkill";
+
+export interface JobProps {
+  Title: string;
+  Company: string;
+  Location: string;
+  JobType: string;
+  SalaryRange: string;
+  Skills: ISkill;
+}
