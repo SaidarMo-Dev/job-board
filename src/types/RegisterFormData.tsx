6a4d136dc465e4decfcce491d7a85bcee6ebfee0
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-13 11:18:12 +0200

    Add Register form interface

---
+++ src/types/RegisterFormData.tsx
@@ -0,0 +1,9 @@
+export default interface RegisterFormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+  role: string;
+}
