f97862a4e4d030f091b82902476897eca99fe863
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-17 23:44:52 +0200

    add popular categories ui

---
+++ src/components/PopularCategories.tsx
@@ -0,0 +1,34 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const categories = [
+  { id: 1, category: "Engineering" },
+  { id: 2, category: "Design" },
+  { id: 3, category: "Product" },
+  { id: 4, category: "Marketing" },
+  { id: 5, category: "Sales" },
+  { id: 6, category: "Data" },
+];
+
+export default function PopularCategories() {
+  return (
+    <Card className="md:w-64">
+      <CardHeader>
+        <CardTitle className="text-xl font-bold">Popular Categories</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-2">
+          {categories.map((cat) => {
+            return (
+              <button
+                key={cat.id}
+                className="block text-left w-full text-sm text-gray-500 py-2 px-3 hover:bg-gray-100 cursor-pointer"
+              >
+                {cat.category}
+              </button>
+            );
+          })}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
