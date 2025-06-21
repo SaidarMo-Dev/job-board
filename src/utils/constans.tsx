d831dabc94caf94c11e40ecb4ae32b87070dc0e2
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-21 23:04:21 +0200

    Add constants

---
+++ src/utils/constans.tsx
@@ -0,0 +1,4 @@
+// utils/constants.ts
+export const ACCESS_TOKEN_KEY = "accessToken";
+export const REFRESH_TOKEN_KEY = "refreshToken";
+export const REFRESH_TOKEN_EXP_KEY = "refreshTokenExpirationDate";
