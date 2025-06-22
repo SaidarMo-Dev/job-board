1d3fcd4386785d40f79567e28ec6ce0ff856c2c1
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-22 15:27:11 +0200

    Create store

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,25 @@ import "./App.css";
 import { ToastContextProvider } from "./contexts/ToastContext";
 import { ToastContainer, Zoom } from "react-toastify";
 import AppRoutes from "./routes/AppRoutes";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="">
-      <ToastContextProvider>
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          theme="colored"
-          transition={Zoom}
-          hideProgressBar={true}
-        />
-        <AppRoutes />
-      </ToastContextProvider>
-    </div>
+    <Provider store={store}>
+      <div className="">
+        <ToastContextProvider>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            theme="colored"
+            transition={Zoom}
+            hideProgressBar={true}
+          />
+          <AppRoutes />
+        </ToastContextProvider>
+      </div>
+    </Provider>
   );
 }
 
