d6c45a2182d39bdd1170699ad442aed44c3285f0
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-25 22:56:11 +0200

    Add profile header

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,31 +1,22 @@
 import AccountSettings from "@/components/AccountSettings";
 import BasicUserInfoCard from "@/components/BasicUserInfoCard";
-import { Button } from "@/components/ui/button";
 
-import { Share2 } from "lucide-react";
 import AuthGuard from "./Auth/authGuard";
+import ProfileCarHeader from "@/components/BasicUserCardComponents/ProfileCardHeader";
+import UserProfileHeader from "@/components/BasicUserCardComponents/UserProfileHeader";
 
 export default function UserProfilePage() {
   return (
     <AuthGuard>
       <div className="min-h-screen bg-gray-50 p-4 md:p-8">
-        <div className="custom-container space-y-6">
-          {/* Header */}
-          <div className="flex items-center justify-between">
-            <div>
-              <h1 className="text-3xl font-bold text-gray-900">Profile</h1>
-              <p className="text-gray-600 mt-1">
-                Manage your personal information and preferences
-              </p>
-            </div>
-            <Button variant="outline" className="flex items-center gap-2">
-              <Share2 className="h-4 w-4" />
-              Share profile
-            </Button>
+        <div className="custom-container">
+          <div className="space-y-6 md:max-w-[850px] lg:w-[850px] m-auto">
+            {/* Header */}
+            <UserProfileHeader />
+            <ProfileCarHeader />
+            <BasicUserInfoCard />
+            <AccountSettings />
           </div>
-
-          <BasicUserInfoCard />
-          <AccountSettings />
         </div>
       </div>
     </AuthGuard>
