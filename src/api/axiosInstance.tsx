477624f8755cc690b2b3212f3014554b2097085f
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-25 16:13:28 +0200

    Update axios instance

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -1,3 +1,4 @@
+import { ACCESS_TOKEN_KEY } from "@/utils/constans";
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -6,5 +7,9 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+axiosInstance.defaults.headers.common["Authorization"] =
+  localStorage.getItem(ACCESS_TOKEN_KEY);
+axiosInstance.defaults.timeout = 3000;
 
 export default axiosInstance;
