6fbe83449fac5c85acf1c88632f2e8da2677c41d
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-06-28 23:42:38 +0200

    add nprogress

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -1,5 +1,26 @@
 import { getAccessToken } from "@/utils/gitAccessToken";
 import axios from "axios";
+import NProgress from "nprogress";
+
+NProgress.configure({
+  showSpinner: false,
+});
+
+// Track how many requests are active
+let requestCount = 0;
+
+const startProgress = () => {
+  if (requestCount === 0) NProgress.start();
+  requestCount++;
+};
+
+const stopProgress = () => {
+  requestCount--;
+  if (requestCount <= 0) {
+    NProgress.done();
+    requestCount = 0;
+  }
+};
 
 const axiosInstance = axios.create({
   baseURL: "https://localhost:7235/Api/V1",
@@ -7,6 +28,33 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getAccessToken();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    startProgress();
+    return config;
+  },
+  (error) => {
+    stopProgress();
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    stopProgress();
+    return response;
+  },
+  (error) => {
+    stopProgress();
+    return Promise.reject(error);
+  }
+);
 
 axiosInstance.defaults.headers.common[
   "Authorization"
