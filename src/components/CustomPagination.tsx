0d6b12ee7bce7a0c9becc41b0c16047c5eb9e852
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-07-03 21:40:26 +0200

    Add custom pagination

---
+++ src/components/CustomPagination.tsx
@@ -0,0 +1,90 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { useState } from "react";
+
+interface CustomPaginationProps {
+  onChange: (page) => void;
+}
+export default function CustomPagination({ onChange }: CustomPaginationProps) {
+  const [selectedPage, setSelectedPage] = useState(1);
+
+  function handlePrevious() {
+    if (selectedPage !== 1) {
+      const prevPage = selectedPage - 1;
+      setSelectedPage(prevPage);
+      onChange(prevPage);
+    }
+  }
+
+  function handleSelectPage(page: number) {
+    if (page !== selectedPage) {
+      setSelectedPage(page);
+      onChange(page);
+    }
+  }
+
+  function handleNext() {
+    const nextPage = selectedPage + 1;
+    setSelectedPage(nextPage);
+    onChange(nextPage);
+  }
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem onClick={handlePrevious} className="cursor-pointer">
+          <PaginationPrevious />
+        </PaginationItem>
+        {selectedPage === 1 ? (
+          <>
+            <PaginationItem>
+              <PaginationLink
+                isActive
+                onClick={() => handleSelectPage(selectedPage)}
+              >
+                {selectedPage}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                onClick={() => handleSelectPage(selectedPage + 1)}
+              >
+                {selectedPage + 1}
+              </PaginationLink>
+            </PaginationItem>
+          </>
+        ) : (
+          <>
+            <PaginationItem>
+              <PaginationLink
+                onClick={() => handleSelectPage(selectedPage - 1)}
+              >
+                {selectedPage - 1}
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                isActive
+                onClick={() => handleSelectPage(selectedPage)}
+              >
+                {selectedPage}
+              </PaginationLink>
+            </PaginationItem>
+          </>
+        )}
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem onClick={handleNext} className="cursor-pointer">
+          <PaginationNext />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
