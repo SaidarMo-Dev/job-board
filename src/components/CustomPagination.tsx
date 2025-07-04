c7efa2917f4ab7cd6cd892c2478d09c41f8315fb
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-07-04 22:25:19 +0200

    Handle pagination logic

--- src/components/CustomPagination.tsx
+++ src/components/CustomPagination.tsx
@@ -7,14 +7,25 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
-import { useState } from "react";
+import { selectHasNextPage } from "@/features/jobs/jobSlice";
+import type { RootState } from "@/store";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 interface CustomPaginationProps {
   onChange: (page) => void;
 }
 export default function CustomPagination({ onChange }: CustomPaginationProps) {
   const [selectedPage, setSelectedPage] = useState(1);
+  const hasNextPage = useSelector(selectHasNextPage);
+  const currentPage = useSelector(
+    (state: RootState) => state.jobReducer.currentPage
+  );
 
+  useEffect(() => {
+    setSelectedPage(currentPage);
+  }, [currentPage]);
+  
   function handlePrevious() {
     if (selectedPage !== 1) {
       const prevPage = selectedPage - 1;
@@ -24,16 +35,23 @@ export default function CustomPagination({ onChange }: CustomPaginationProps) {
   }
 
   function handleSelectPage(page: number) {
-    if (page !== selectedPage) {
+    if (page !== selectedPage && page > selectedPage) {
+      if (hasNextPage) {
+        setSelectedPage(page);
+        onChange(page);
+      }
+    } else if (page !== selectedPage && page < selectedPage) {
       setSelectedPage(page);
       onChange(page);
     }
   }
 
   function handleNext() {
-    const nextPage = selectedPage + 1;
-    setSelectedPage(nextPage);
-    onChange(nextPage);
+    if (hasNextPage) {
+      const nextPage = selectedPage + 1;
+      setSelectedPage(nextPage);
+      onChange(nextPage);
+    }
   }
   return (
     <Pagination>
