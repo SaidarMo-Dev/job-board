b25404f55e779f6aebc92445873a6abb57e6a1a1
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-07-04 23:11:43 +0200

    Add skill badge

---
+++ src/components/SkillBadge.tsx
@@ -0,0 +1,26 @@
+import type { SkillResponse } from "@/types/Skill";
+import { Badge } from "./ui/badge";
+
+interface SkillBadgeProps {
+  index: number;
+  skill: SkillResponse;
+  className?: string;
+}
+export default function SkillBadge({
+  skill,
+  index,
+  className = "",
+}: SkillBadgeProps) {
+  return (
+    <Badge
+      key={skill.id}
+      variant="secondary"
+      className={`px-4 py-2 text-sm font-medium bg-white dark:bg-slate-800 border border-gray-200 dark:border-gray-700 hover:bg-blue-50 dark:hover:bg-blue-900/20 hover:border-blue-300 dark:hover:border-blue-600 transition-all duration-300 hover:scale-105 cursor-default ${className} `}
+      style={{
+        animationDelay: `${index * 100}ms`,
+      }}
+    >
+      {skill.name}
+    </Badge>
+  );
+}
