642e4861e15ada6d99947fba59a6580c42fe30de
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-07-10 20:58:55 +0200

    Add steps

---
+++ src/features/jobApplications/components/ApplicationNavigation.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight, CheckCircle } from "lucide-react";
+
+interface ApplicationNavigationProps {
+  currentStep: number;
+  isStepValid: boolean;
+  onNextStep: () => void;
+  onPrevStep: () => void;
+  onSubmit: () => void;
+  isLastStep?: boolean;
+}
+
+export default function ApplicationNavigation({
+  currentStep,
+  isStepValid,
+  onNextStep,
+  onPrevStep,
+  onSubmit,
+  isLastStep = false,
+}: ApplicationNavigationProps) {
+  return (
+    <div className="flex justify-between pt-4 w-full">
+      <Button
+        variant="outline"
+        className="flex items-center gap-2 bg-transparent md:w-25"
+        disabled={currentStep === 0}
+        onClick={onPrevStep}
+      >
+        <ArrowLeft className="h-4 w-4" />
+        Previos
+      </Button>
+
+      {isLastStep ? (
+        <Button
+          onClick={onSubmit}
+          disabled={!isStepValid}
+          className="flex items-center gap-2"
+        >
+          Submit Application
+          <CheckCircle className="h-4 w-4" />
+        </Button>
+      ) : (
+        <Button
+          onClick={onNextStep}
+          disabled={!isStepValid}
+          className="flex items-center gap-2 md:w-25"
+        >
+          Next
+          <ArrowRight className="h-4 w-4" />
+        </Button>
+      )}
+    </div>
+  );
+}
--- src/features/jobApplications/components/JobApplicationWizard.tsx
+++ src/features/jobApplications/components/JobApplicationWizard.tsx
@@ -1,40 +1,126 @@
 import { type JobResponse } from "@/features/jobs/jobTypes";
 import ApplicationHeader from "./ApplicationHeader";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import type { ApplicationData } from "../applicationType";
+import PersonalInfoStep from "./PersonalInfoStep";
+import { DocumentsStep } from "./DocumentStep";
+import { AdditionalInfoStep } from "./AdditionalInfoStep";
+import { ReviewStep } from "./ReviewStep";
+import { SuccessStep } from "./SuccessStep";
+import ApplicationNavigation from "./ApplicationNavigation";
+import ProgressBar from "@/components/ProgressBar";
 
-const job: JobResponse = {
-  jobId: 1,
-  title: "Test job",
-  description: "test Description",
-  companyName: "Hello",
-  location: "El Jadida",
-  jobType: "Full Type",
-  maxSalary: 120000,
-  minSalary: 130000,
-  experienceLevel: "EntryLevel",
-  datePosted: new Date(),
-  status: "Pending",
-  skills: [],
-  cretaedByUser: "Mohammed",
-  categories: [],
-};
+interface ApplicationFormProps {
+  currentStep: number;
+  applicationData: ApplicationData;
+  jobData: JobResponse;
+  onInputChange: (field: keyof ApplicationData, value: string) => void;
+  onFileUpload: (file: File | null) => void;
+  onPrevStep: () => void;
+  onNextStep: () => void;
+  onSubmit: () => void;
+  onReset: () => void;
+  isStepValid: boolean;
+}
 
-export default function JobApplicationWizard() {
+const steps = [
+  "Personal Info",
+  "Documents",
+  "Additional Info",
+  "Review",
+  "Complete",
+];
+
+export default function JobApplicationWizard({
+  currentStep,
+  applicationData,
+  jobData,
+  onInputChange,
+  onFileUpload,
+  onPrevStep,
+  onNextStep,
+  onSubmit,
+  onReset,
+  isStepValid,
+}: ApplicationFormProps) {
+  const renderStepContent = () => {
+    switch (currentStep) {
+      case 0:
+        return (
+          <PersonalInfoStep
+            applicationData={applicationData}
+            onInputChange={onInputChange}
+          />
+        );
+      case 1:
+        return (
+          <DocumentsStep
+            applicationData={applicationData}
+            onFileUpload={onFileUpload}
+            onInputChange={onInputChange}
+          />
+        );
+
+      case 2:
+        return (
+          <AdditionalInfoStep
+            applicationData={applicationData}
+            onInputChange={onInputChange}
+          />
+        );
+
+      case 3:
+        return <ReviewStep applicationData={applicationData} />;
+
+      case 4:
+        return <SuccessStep jobData={jobData} onReset={onReset} />;
+
+      default:
+        return null;
+    }
+  };
+
+  if (currentStep === 4) {
+    return (
+      <Card>
+        <CardContent className="pt-6">{renderStepContent()}</CardContent>
+      </Card>
+    );
+  }
   return (
     <div className="max-w-4xl m-auto space-y-5">
       {/* Application flow header */}
-      <ApplicationHeader jobData={job} />
+      <ApplicationHeader jobData={jobData} />
       <Card>
         {/* Steps */}
         <CardHeader>
           <div className="flex items-center justify-between">
-            <CardTitle>Application - Step 1 of 4</CardTitle>
-            <span className="text-sm text-gray-500">Personal info</span>
+            <CardTitle>Application - Step {currentStep + 1} of 4</CardTitle>
+            <span className="text-sm text-gray-500">{steps[currentStep]}</span>
           </div>
-          <Progress value={(1 / 4) * 100} className="mt-2" />
+          <ProgressBar
+            value={(1 / (steps.length - 1)) * 100}
+            className="mt-2 h-[10px]"
+            color=""
+          />
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>{renderStepContent()}</CardContent>
+        <CardFooter>
+          <ApplicationNavigation
+            currentStep={currentStep}
+            onNextStep={onNextStep}
+            onPrevStep={onPrevStep}
+            onSubmit={onSubmit}
+            isStepValid={isStepValid}
+            isLastStep={currentStep === 3}
+          />
+        </CardFooter>
       </Card>
     </div>
   );
