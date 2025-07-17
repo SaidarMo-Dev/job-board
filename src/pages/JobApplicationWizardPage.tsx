ab804a1b9ec4ee1fd5035facf76fb5ebf09f6437
SaidarMo-Dev <mohamalikdg@gmail.com>
2025-07-17 22:53:57 +0200

    Complete job application flow logic

--- src/pages/JobApplicationWizardPage.tsx
+++ src/pages/JobApplicationWizardPage.tsx
@@ -1,24 +1,11 @@
 import JobApplicationWizard from "@/features/jobApplications/components/JobApplicationWizard";
+import { getJobByIdThunk } from "@/features/jobs/jobThunk";
 import type { JobResponse } from "@/features/jobs/jobTypes";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
 import { useApplicationWizard } from "@/hooks/useApplicationWizard";
-
-const jobData: JobResponse = {
-  jobId: 1,
-  title: "Frontend Developer",
-  description:
-    "We are looking for a skilled frontend developer to join our team.",
-  companyName: "TechCorp",
-  location: "Stockholm, Sweden",
-  jobType: "Full-time",
-  maxSalary: 70000,
-  minSalary: 50000,
-  experienceLevel: "Any",
-  datePosted: new Date("2025-07-01"),
-  status: "Open",
-  skills: [],
-  cretaedByUser: "admin@example.com",
-  categories: [],
-};
+import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router";
+import { toast } from "react-toastify";
 
 export default function JobApplicationWizardPage() {
   const {
@@ -32,10 +19,48 @@ export default function JobApplicationWizardPage() {
     isStepValid,
     resetForm,
   } = useApplicationWizard();
+
+  const { jobId } = useParams();
+  const hasInitialized = useRef(false);
+
+  const [jobInfo, setJobInfo] = useState<JobResponse>({
+    jobId: -1,
+    title: "",
+    description: "",
+    companyName: "",
+    location: "",
+    jobType: "",
+    maxSalary: 0,
+    minSalary: 0,
+    experienceLevel: "Any",
+    datePosted: new Date(),
+    status: "",
+    skills: [],
+    cretaedByUser: "",
+    categories: [],
+  });
+
+  const dispatch = useAppDispatch();
+
+  if (!hasInitialized.current && jobId) {
+    handleInputChange("jobId", jobId ?? "-1");
+    hasInitialized.current = true;
+  }
+
+  useEffect(() => {
+    dispatch(getJobByIdThunk({ Id: Number(jobId ?? -1) })).then((result) => {
+      if (getJobByIdThunk.fulfilled.match(result)) {
+        setJobInfo(result.payload);
+      } else {
+        toast.error(result.payload);
+      }
+    });
+  }, [dispatch, jobId]);
+
   return (
     <div className="custom-container">
       <JobApplicationWizard
-        jobData={jobData}
+        jobData={jobInfo}
         currentStep={currentStep}
         applicationData={applicationData}
         onInputChange={handleInputChange}
